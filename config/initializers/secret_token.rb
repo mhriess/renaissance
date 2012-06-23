249528009a8aee45dd88a846f95da54e3635ba02
Mike Riess <mikeriess@gmail.com>
2012-06-23 15:54:03 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Renaissance::Application.config.secret_token = '7853094e1d02d5fa654afcce4fe46666d904f96cad26d3426dacda8bd3056e48e744fb38a2b27b30dab9fcb53387ffc66c564223c1ce85918b7af33f5b397174'
